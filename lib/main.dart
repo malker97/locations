0b1e8884f949387309f678c01595224553f173b3
Zhengmao Zhang <malker@qq.com>
2023-04-16 04:21:36 -0700

    use OpenStreeMap API to get speed limit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
-void main() {
+import 'package:http/http.dart' as http;
+
+void main(){
   runApp(const MyApp());
 }
 
@@ -28,24 +31,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   LocationData? _currentLocation;
   StreamSubscription<LocationData>? _locationSubscription;
+  double lat = 0.0;
+  double long = 0.0;
+  String _speedLimit = "";
   void getCurrentLocation(){
     Location location = Location();
     location.getLocation().then((value) => setState(() {
       _currentLocation = value;
-      print(_currentLocation!.latitude);
+      // print(_currentLocation!.latitude);
     }));
   }
+  Future<void> getSpeedLimit(double lat, double lng) async {
+    // http://www.overpass-api.de/api/interpreter?data=[out:json];way[%22maxspeed%22](around:10,48.071615,7.338893);out;
+    var url = Uri.parse('http://www.overpass-api.de/api/interpreter?data=[out:json];way[%22maxspeed%22](around:10,$lat,$lng);out;');
+    var response = await http.get(url);
+    var speedLimit = "";
+    if(response.statusCode == 200){
+      var body = jsonDecode(response.body);
+      if(body['elements'][0]['tags']['maxspeed'] != null){
+        print(body['elements'][0]['tags']['maxspeed']);
+        _speedLimit = body['elements'][0]['tags']['maxspeed'];
+        // speedLimit = int.parse(body['elements'][0]['tags']['maxspeed']);
+        // return speedLimit;
+        // return int.parse(body['elements'][0]['tags']['maxspeed']);
+      }
+    }
+  }
+
   Future<void> _listenCurrentLocation() async {
     Location location = Location();
     _locationSubscription = location.onLocationChanged.listen((LocationData currentLocation) {
       setState(() {
         _currentLocation = currentLocation;
+        lat = _currentLocation!.latitude!;
+        long = _currentLocation!.longitude!;
+        print("lat: $lat, long: $long");
       });
     });
+    Timer.periodic(Duration(seconds: 1), (timer) async {
+      await getSpeedLimit(lat, long);
+      print('Current speed limit: $_speedLimit');
+    });
   }
+  
   @override
   void initState() {
     super.initState();
@@ -72,6 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     Text('Altitude: ${_currentLocation!.altitude}'),
                     Text('Speed: ${_currentLocation!.speed}'),
                     Text('Speed Accuracy: ${_currentLocation!.speedAccuracy}'),
+                    Text('Speed Limit: $_speedLimit')
                   ],
                 ),
           ],
